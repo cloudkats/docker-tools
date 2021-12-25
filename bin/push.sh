27deaad763568ac800a93d3e8bb5760e3285849b
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2021-12-25 23:29:21 +0000

    always push images

--- bin/push.sh
+++ bin/push.sh
@@ -12,17 +12,24 @@ echo "push version ${IMAGE_ID}:latest"
 docker tag "${IMAGE}" "${IMAGE_ID}:latest"
 docker push "${IMAGE_ID}:latest"
 
-echo "push version ${IMAGE_ID}:${TAG_VERSION_DATE}"
-docker tag "${IMAGE}" "${IMAGE_ID}:${TAG_VERSION_DATE}"
-docker push "${IMAGE_ID}:${TAG_VERSION_DATE}"
-
-echo "push version ${IMAGE_ID}:${TAG_VERSION_SHA}"
-
-docker tag "${IMAGE}" "${IMAGE_ID}:${TAG_VERSION_SHA}"
-docker push "${IMAGE_ID}:${TAG_VERSION_SHA}"
-
 if [ "$IMAGE_VERSION" != "latest" ]; then
   echo "push version ${IMAGE_ID}:${IMAGE_VERSION}"
   docker tag "${IMAGE}" "${IMAGE_ID}:${IMAGE_VERSION}"
   docker push "${IMAGE_ID}:${IMAGE_VERSION}"
+
+  echo "push version ${IMAGE_ID}:${IMAGE_VERSION}.${TAG_VERSION_DATE}"
+  docker tag "${IMAGE}" "${IMAGE_ID}:${IMAGE_VERSION}.${TAG_VERSION_DATE}"
+  docker push "${IMAGE_ID}:${IMAGE_VERSION}.${TAG_VERSION_DATE}"
+
+  echo "push version ${IMAGE_ID}:${IMAGE_VERSION}.${TAG_VERSION_SHA}"
+  docker tag "${IMAGE}" "${IMAGE_ID}:${IMAGE_VERSION}.${TAG_VERSION_SHA}"
+  docker push "${IMAGE_ID}:${IMAGE_VERSION}.${TAG_VERSION_SHA}"
+else
+  echo "push version ${IMAGE_ID}:${TAG_VERSION_DATE}"
+  docker tag "${IMAGE}" "${IMAGE_ID}:${TAG_VERSION_DATE}"
+  docker push "${IMAGE_ID}:${TAG_VERSION_DATE}"
+
+  echo "push version ${IMAGE_ID}:${TAG_VERSION_SHA}"
+  docker tag "${IMAGE}" "${IMAGE_ID}:${TAG_VERSION_SHA}"
+  docker push "${IMAGE_ID}:${TAG_VERSION_SHA}"
 fi
