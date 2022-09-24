91577f061a380ed98208d1ed37b8db7519997209
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2022-09-24 13:43:06 +0100

    tfm should not have terragrunt

--- terraform/test.sh
+++ terraform/test.sh
@@ -5,7 +5,6 @@ set -e
 : "${IMAGE}"
 
 docker run --rm --entrypoint="/bin/sh" "${IMAGE}" -c "terraform version"
-docker run --rm --entrypoint="/bin/sh" "${IMAGE}" -c "terragrunt --version"
 docker run --rm --entrypoint="/bin/sh" "${IMAGE}" -c "bash --version"
 docker run --rm --entrypoint="/bin/sh" "${IMAGE}" -c "python --version"
 docker run --rm --entrypoint="/bin/sh" "${IMAGE}" -c "yq --version"
